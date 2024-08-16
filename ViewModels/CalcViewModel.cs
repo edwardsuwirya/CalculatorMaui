cea060552355bc25d644a0b094de2559ee28ce31
Edward Suwirya <edward_suwirya@yahoo.com>
2024-08-16 09:15:33 +0700

    Change logic viewmodel

--- ViewModels/CalcViewModel.cs
+++ ViewModels/CalcViewModel.cs
@@ -9,11 +9,11 @@ public class CalcViewModel : INotifyPropertyChanged
     public event PropertyChangedEventHandler? PropertyChanged;
     private string _displayText = "0";
     private string _operator = "";
-    private string _calcOpr = "";
     private bool _isFinish;
+    private bool _isLoading;
 
-    private int? num1;
-    private int? num2;
+    private int _num1 = 0;
+    private int _num2 = 0;
 
     public string DisplayText
     {
@@ -25,35 +25,32 @@ public class CalcViewModel : INotifyPropertyChanged
         }
     }
 
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set
+        {
+            _isLoading = value;
+            OnPropertyChanged();
+        }
+    }
+
     public string Operator
     {
         get => _operator;
         set
         {
-            if (num1 != null)
-            {
-                _operator = value;
-                if (!_operator.Equals("=")) return;
-                num2 = int.Parse(DisplayText);
-                Calculation();
-                Reset();
-                _isFinish = true;
-            }
-            else
-            {
-                if (!value.Equals("="))
-                {
-                    num1 = int.Parse(DisplayText);
-                    DisplayText = "0";
-                    _calcOpr = value;
-                }
-            }
+            _num1 = int.Parse(DisplayText);
+            DisplayText = "0";
+            _operator = value;
         }
     }
 
 
     public ICommand DigitCommand { get; private set; }
     public ICommand OperatorCommand { get; private set; }
+    public ICommand CalculateCommand { get; private set; }
+
 
     private void OnPropertyChanged([CallerMemberName] string name = "") =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
@@ -76,29 +73,32 @@ public class CalcViewModel : INotifyPropertyChanged
         });
 
         OperatorCommand = new Command<string>((opr) => { Operator = opr; });
+
+        CalculateCommand = new Command(() =>
+        {
+            _num2 = int.Parse(DisplayText);
+            DisplayText = Calculate(_num1, _num2, Operator).ToString();
+            Reset();
+            _isFinish = true;
+        });
     }
 
     private void Reset()
     {
-        num1 = null;
-        num2 = null;
+        _num1 = 0;
+        _num2 = 0;
         _operator = "";
-        _calcOpr = "";
     }
-
-    private void Calculation()
+    
+    private int Calculate(int value1, int value2, string mathOperator)
     {
-        var calcResult = 0;
-        if (!num1.HasValue || !num2.HasValue) return;
-        calcResult = _calcOpr switch
+        return mathOperator switch
         {
-            "+" => num1.Value + num2.Value,
-            "-" => num1.Value - num2.Value,
-            "/" => num1.Value / num2.Value,
-            "*" => num1.Value * num2.Value,
-            _ => calcResult
+            "+" => value1 + value2,
+            "-" => value1 - value2,
+            "/" => value1 / value2,
+            "*" => value1 * value2,
+            _ => throw new ArgumentOutOfRangeException()
         };
-
-        DisplayText = calcResult.ToString();
     }
 }
