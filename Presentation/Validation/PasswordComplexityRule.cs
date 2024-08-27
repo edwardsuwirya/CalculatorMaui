0a200bb6f093947971025380a305bea051402d07
Edward Suwirya <edward_suwirya@yahoo.com>
2024-08-27 07:51:42 +0700

    Using Linq

--- Presentation/Validation/PasswordComplexityRule.cs
+++ Presentation/Validation/PasswordComplexityRule.cs
@@ -7,17 +7,24 @@ public class PasswordComplexityRule<T> : IValidationRule<T>
     public bool CheckValidity(T value)
     {
         var sPassword = value as string ?? string.Empty;
-        var charArr = sPassword.ToCharArray();
-        var isCharSymbolExist = false;
-        var isNumericExist = false;
-        var isUppercaseExist = false;
-        foreach (var c in charArr)
-        {
-            if ("!@#$%^&*()_+=-?/.,<>:;'{}[]".Contains(c)) isCharSymbolExist = true;
-            if ("1234567890".Contains(c)) isNumericExist = true;
-            if (char.IsUpper(c)) isUppercaseExist = true;
-        }
+        var stringSymbol = "!@#$%^&*()_+=-?/.,<>:;'{}[]\"";
+        // var charArr = sPassword.ToCharArray();
+        // var isCharSymbolExist = false;
+        // var isNumericExist = false;
+        // var isUppercaseExist = false;
+        // foreach (var c in charArr)
+        // {
+        //     if ("!@#$%^&*()_+=-?/.,<>:;'{}[]".Contains(c)) isCharSymbolExist = true;
+        //     if ("1234567890".Contains(c)) isNumericExist = true;
+        //     if (char.IsUpper(c)) isUppercaseExist = true;
+        // }
+        //
+        // return isNumericExist && isCharSymbolExist && isUppercaseExist;
+        // Using Linq
+        var passwordSymbolExist = from ch in sPassword where stringSymbol.Contains(ch) select ch;
+        var passwordNumericIsExist = from ch in sPassword where char.IsDigit(ch) select ch;
+        var passwordUpperCaseIsExist = from ch in sPassword where char.IsUpper(ch) select ch;
 
-        return isNumericExist && isCharSymbolExist && isUppercaseExist;
+        return passwordSymbolExist.Any() && passwordNumericIsExist.Any() && passwordUpperCaseIsExist.Any();
     }
 }
